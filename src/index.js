6c77724dc1506aadc99932af1f7ad48c289ab0d6
Mihaiela Nemes <mihaiela.nemes@stellenanzeigen.de>
2020-10-18 19:56:08 +0200

    First react app with examples

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-    const now = new Date();
-    const a = 10;
-    const b = 20;
-
+const Hello = (props) => {
     return (
         <div>
-            <p>Hello world, it is {now.toString()}</p>
-            <p>
-                {a} plus {b} is {a + b}
-            </p>
+            <p>Hello {props.name}, you are {props.age} years old</p>
         </div>
     )
-}
+};
+
+const Footer = () => {
+    return (
+        <div>
+            greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+        </div>
+    )
+};
+
+const App = () => {
+    const name = 'Peter';
+    const age = 10;
+    return (
+        <>
+            <h1>Greetings</h1>
+            <Hello name="Mia" age={26 + 10}/>
+            <Hello name={name} age={age}/>
+            <Footer />
+        </>
+    )
+};
 
 ReactDOM.render(<App />, document.getElementById('root'))
