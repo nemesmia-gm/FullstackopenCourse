e1c506504dc3cfc6ac9b4b0863abe2793bd556a8
Mihaiela Nemes <mihaiela.nemes@stellenanzeigen.de>
2020-10-19 10:03:33 +0200

    courseinfo step 2

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -7,9 +7,18 @@ const Header = (props) => {
 )
 };
 
+const Part = (props) => {
+    return (
+        <p>Content {props.part},{props.exercise} </p>
+    )
+};
 const Content = (props) => {
     return (
-            <p>Content {props.part},{props.exercise} </p>
+            <div>
+                <Part part={props.part1} exercise={props.exercises1}/>
+                <Part part={props.part2} exercise={props.exercises2} />
+                <Part part={props.part3} exercise={props.exercises3} />
+            </div>
     )
 };
 
@@ -32,10 +41,11 @@ const App = () => {
   return (
       <div>
           <Header course={course} />
-
-          <Content part={part1} exercise={exercises1} />
-          <Content part={part2} exercise={exercises2} />
-          <Content part={part3} exercise={exercises3} />
+          <Content
+              part1={part1} exercise1={exercises1}
+              part2={part2} exercise2={exercises2}
+              part3={part3} exercise3={exercises3}
+          />
           <Total total={exercises1 + exercises2 + exercises3} />
   </div>
 )
